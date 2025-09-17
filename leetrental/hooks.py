19ab1445d5d1afa32cc67e27bcba4b080207b364
Leet IT Solutions <91686534+1337it@users.noreply.github.com>
2025-09-18 01:49:17 +0400

    Update hooks.py

--- leetrental/hooks.py
+++ leetrental/hooks.py
@@ -32,7 +32,7 @@ override_doctype_dashboards = {
 app_include_css = "/assets/leetrental/css/leetrental.css"
 app_include_js = "/assets/leetrental/js/leetrental.js"
 
-app_include_html = "/assets/leetrental/icons/rental-icons.svg"
+app_include_icons = "/assets/leetrental/icons/rental-icons.svg"
 # include js, css files in header of web template
 # web_include_css = "/assets/leetrental/css/leetrental.css"
 # web_include_js = "/assets/leetrental/js/leetrental.js"
