a04955ef9384cdece6c3b7da4f90c6467299b53e
Leet IT Solutions <91686534+1337it@users.noreply.github.com>
2025-09-27 15:05:09 +0400

    Update vehicle_listview.js

--- leetrental/public/js/vehicle_listview.js
+++ leetrental/public/js/vehicle_listview.js
@@ -1,5 +1,5 @@
 // leetrental/public/js/vehicle_listview.js
-frappe.listview_settings["Vehicle"] = {
+frappe.listview_settings["Vehicles"] = {
   onload(listview) {
     listview.page.add_menu_item(__("Open Vehicle Kanban"), () => frappe.set_route("vehicle-kanban"));
   },
