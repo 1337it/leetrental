dfa5fc535925e3e559aa501504366287e60292a3
Leet IT Solutions <91686534+1337it@users.noreply.github.com>
2025-09-29 17:18:39 +0400

    Delete leetrental/public/js/vehicle_listview.js

--- leetrental/public/js/vehicle_listview.js
+++
@@ -1,6 +0,0 @@
-// leetrental/public/js/vehicle_listview.js
-frappe.listview_settings["Vehicles"] = {
-  onload(listview) {
-    listview.page.add_menu_item(__("Open Vehicle Kanban"), () => frappe.set_route("vehicle-kanban"));
-  },
-};
