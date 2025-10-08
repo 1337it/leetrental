04cf8212b636ae2650a890ba4157d95b648ccc27
Leet IT Solutions <91686534+1337it@users.noreply.github.com>
2025-10-08 07:06:07 +0400

    Update hooks.py

--- leetrental/hooks.py
+++ leetrental/hooks.py
@@ -41,14 +41,6 @@ app_include_js = [
 
 app_include_icons = "leetrental/icons/rental-icons.svg"
 
-
-fixtures = [
-    "Role",
-    "Workflow State",
-    "Workflow Action Master",
-    "Custom Field",
-    "Workflow"
-]
 # hooks.py
 
 # If you also want this in Desk (optional):
